554117ea63eb2066392b8a48287fe41e746cdf48
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-02-20 17:08:23 -0500

    started mobile responsiveness

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,46 +9,42 @@ import RJShoemakerImage from '../assets/images/RJ_Shoemaker_Image.png';
 const Home = () => {
   return (
     <>
-      <div className='home-wrapper'>
-        <Navbar />
-        <div className='home-content'>
-          <div className='home-header'>
-            Websites that are out of this world.
+      <Navbar />
+      <div className='home-content'>
+        <div className='home-header'>Websites that are out of this world.</div>
+        <div className='portfolio-wrapper'>
+          <PortfolioItem
+            id='handmade-audio'
+            url='http://handmade-audio.com'
+            imageUrl={HandmadeAudioImage}
+            imageId='handmade-audio-image'
+            boxLabel='Handmade Audio'
+          />
+          <PortfolioItem
+            id='rj-shoemaker'
+            url='http://www.rjshoemaker.com'
+            target='_blank'
+            imageUrl={RJShoemakerImage}
+            imageId='rj-shoemaker-image'
+            boxLabel="RJ Shoemaker's Website"
+          />
+        </div>
+        <div className='home-header'>No cookie-cutter designs.</div>
+        <div className='portfolio-wrapper'>
+          <BoringWebsite id='boring-website-1' />
+          <BoringWebsite id='boring-website-2' />
+          <BoringWebsite id='boring-website-3' />
+        </div>
+        <div className='home-subheader'>
+          Let us build your personal or small business website.
+        </div>
+        <div className='contact-section'>
+          <div id='contact-header' className='contact-row'>
+            Reach Out:
           </div>
-          <div className='portfolio-wrapper'>
-            <PortfolioItem
-              id='handmade-audio'
-              url='http://handmade-audio.com'
-              imageUrl={HandmadeAudioImage}
-              imageId='handmade-audio-image'
-              boxLabel='Handmade Audio'
-            />
-            <PortfolioItem
-              id='rj-shoemaker'
-              url='http://www.rjshoemaker.com'
-              target='_blank'
-              imageUrl={RJShoemakerImage}
-              imageId='rj-shoemaker-image'
-              boxLabel="RJ Shoemaker's Website"
-            />
-          </div>
-          <div className='home-header'>No cookie-cutter designs.</div>
-          <div className='portfolio-wrapper'>
-            <BoringWebsite id='boring-website-1' />
-            <BoringWebsite id='boring-website-2' />
-            <BoringWebsite id='boring-website-3' />
-          </div>
-          <div className='home-subheader'>
-            Let us build your personal or small business website.
-          </div>
-          <div className='contact-section'>
-            <div id='contact-header' className='contact-row'>
-              Reach Out:
-            </div>
-            <div className='contact-row'>Lunar Web Development</div>
-            <div className='contact-row'>rj.shoemaker@lunardm.com</div>
-            <div className='contact-row'>908-824-0852</div>
-          </div>
+          <div className='contact-row'>Lunar Web Development</div>
+          <div className='contact-row'>rj.shoemaker@lunardm.com</div>
+          <div className='contact-row'>908-824-0852</div>
         </div>
       </div>
     </>
