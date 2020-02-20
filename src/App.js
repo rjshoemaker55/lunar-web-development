57e2ab15d083b0e5d0869245e45310991d9f9f30
rjshoemaker55 <rjshoemaker55@gmail.com>
2020-02-20 16:35:26 -0500

    added heroku

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Landing from './pages/Landing';
 import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Route exact path='/' component={Landing} />
-      <Route exact path='/home' component={Home} />
+      <Route exact path='/' component={Home} />
     </Router>
   );
 }
